03ef4a8a5b73cf58c3826a6537f7cdf8874e17be
Jorge Saa <piojo.sistematico@gmail.com>
2023-10-16 20:28:43 -0300

    basic html markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,223 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import logo from "./assets/images/logo.svg";
+import bgMobile from "./assets/images/bg-intro-mobile.svg";
+import bgDesktop from "./assets/images/bg-intro-desktop.svg";
+import mockups from "./assets/images/image-mockups.png";
+
+import iconOnline from "./assets/images/icon-online.svg";
+import iconBudgeting from "./assets/images/icon-budgeting.svg";
+import iconOnboarding from "./assets/images/icon-onboarding.svg";
+import iconAPI from "./assets/images/icon-api.svg";
+
+import imgCurrency from "./assets/images/image-currency.jpg";
+import imgRestaurant from "./assets/images/image-restaurant.jpg";
+import imgPlane from "./assets/images/image-plane.jpg";
+import imgConfetti from "./assets/images/image-confetti.jpg";
+
+import iconFacebook from "./assets/images/icon-facebook.svg";
+import iconYoutube from "./assets/images/icon-youtube.svg";
+import iconTwitter from "./assets/images/icon-twitter.svg";
+import iconPinterest from "./assets/images/icon-pinterest.svg";
+import iconInstagram from "./assets/images/icon-instagram.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <picture>
+          <img src={logo} alt="hero" />
+        </picture>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+          </ul>
+          <button>Request Invite</button>
+        </nav>
+      </header>
+      <main>
+        <section title="hero">
+          <picture>
+            <img src={mockups} alt="hero" />
+            <img src={bgMobile} alt="" />
+            <img src={bgDesktop} alt="" />
+          </picture>
+          <div>
+            <h1>Next generation digital banking</h1>
+            <p>
+              Take your financial life online. Your Easybank account will be a
+              one-stop-shop for spending, saving, budgeting, investing, and much
+              more.
+            </p>
+            <button>Request Invite</button>
+          </div>
+        </section>
+        <section title="details">
+          <h2>Why choose Easybank?</h2>
+          <p>
+            We leverage Open Banking to turn your bank account into your
+            financial hub. Control your finances like never before.
+          </p>
+          <div>
+            <article>
+              <picture>
+                <img src={iconOnline} alt="" />
+              </picture>
+              <h3>Online Banking</h3>
+              <p>
+                Our modern web and mobile applications allow you to keep track
+                of your finances wherever you are in the world.
+              </p>
+            </article>
+            <article>
+              <picture>
+                <img src={iconBudgeting} alt="" />
+              </picture>
+              <h3>Simple Budgeting</h3>
+              <p>
+                See exactly where your money goes each month. Receive
+                notifications when you’re close to hitting your limits.
+              </p>
+            </article>
+            <article>
+              <picture>
+                <img src={iconOnboarding} alt="" />
+              </picture>
+              <h3>Fast Onboarding</h3>
+              <p>
+                We don’t do branches. Open your account in minutes online and
+                start taking control of your finances right away.
+              </p>
+            </article>
+            <article>
+              <picture>
+                <img src={iconAPI} alt="" />
+              </picture>
+              <h3>Open API</h3>
+              <p>
+                Manage your savings, investments, pension, and much more from
+                one account. Tracking your money has never been easier.
+              </p>
+            </article>
+          </div>
+          <div>
+            <h2>Latest Articles</h2>
+            <article>
+              <picture>
+                <img src={imgCurrency} alt="" />
+              </picture>
+              <span>By Claire Robinson</span>
+              <h4>Receive money in any currency with no fees</h4>
+              <p>
+                The world is getting smaller and we’re becoming more mobile. So
+                why should you be forced to only receive money in a single …
+              </p>
+            </article>
+            <article>
+              <picture>
+                <img src={imgRestaurant} alt="" />
+              </picture>
+              <span>By Wilson Hutton</span>
+              <h4>Treat yourself without worrying about money</h4>
+              <p>
+                Our simple budgeting feature allows you to separate out your
+                spending and set realistic limits each month. That means you …
+              </p>
+            </article>
+            <article>
+              <picture>
+                <img src={imgPlane} alt="" />
+              </picture>
+              <span>By Wilson Hutton</span>
+              <h4>Take your Easybank card wherever you go</h4>
+              <p>
+                We want you to enjoy your travels. This is why we don’t charge
+                any fees on purchases while you’re abroad. We’ll even show you …
+              </p>
+            </article>
+            <article>
+              <picture>
+                <img src={imgConfetti} alt="" />
+              </picture>
+              <span>By Claire Robinson</span>
+              <h4>Our invite-only Beta accounts are now live!</h4>
+              <p>
+                After a lot of hard work by the whole team, we’re excited to
+                launch our closed beta. It’s easy to request an invite through
+                the site ...
+              </p>
+            </article>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div>
+          <picture>
+            <img src={logo} alt="" />
+          </picture>
+          <div>
+            <picture>
+              <img src={iconFacebook} alt="" />
+            </picture>
+            <picture>
+              <img src={iconYoutube} alt="" />
+            </picture>
+            <picture>
+              <img src={iconTwitter} alt="" />
+            </picture>
+            <picture>
+              <img src={iconPinterest} alt="" />
+            </picture>
+            <picture>
+              <img src={iconInstagram} alt="" />
+            </picture>
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+            <li>
+              <a href="#">Support</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <button>Request Invite</button>
+          <span>© Easybank. All Rights Reserved</span>
+        </div>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
